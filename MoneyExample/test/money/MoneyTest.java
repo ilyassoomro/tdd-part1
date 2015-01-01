c76e7a7b910c758987ac2e2b2fbe5b524e35a559
Ilyas Soomro <muhammadilyas_bsel@yahoo.com>
2015-01-01 21:58:47 +0500

    Chapter 2. Degenerate Objects

--- MoneyExample/test/money/MoneyTest.java
+++ MoneyExample/test/money/MoneyTest.java
@@ -7,10 +7,11 @@ import org.junit.Test;
 public class MoneyTest {
 	@Test
 	public void testMultiplication() {
-		Dollar five = new Dollar(10);				// 10
-		five.times(5);								// *5
-		assertEquals(50, five.amount);				// 50  (Should be 50)
-		five.times(10);								// *10
-		assertEquals(500, five.amount);				// 500	(Should be 500)
+		Dollar amount= new Dollar(5);
+		Dollar product= amount.times(2);
+		assertEquals(5, amount.amount);
+		assertEquals(10, product.amount);
+		product= amount.times(3);
+		assertEquals(15, product.amount);
 	}
 }
