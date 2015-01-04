50f4476bcb81a0321aaeab6a30cc6bb1463df164
ilyassoomro <muhammadilyas_bsel@yahoo.com>
2015-01-04 17:54:04 +0500

    Chapter 10. Interesting Times

--- MoneyExample/src/money/Dollar.java
+++ MoneyExample/src/money/Dollar.java
@@ -3,14 +3,9 @@ package money;
 
 public class Dollar extends Money{
 
-	private String currency;
-
 	Dollar(int amount, String currency) {
 		super(amount, currency);
 	}
 
-	Money times(int multiplier) {
-		return Money.dollar(amount * multiplier);
-	}
 
 }
--- MoneyExample/src/money/Franc.java
+++ MoneyExample/src/money/Franc.java
@@ -2,14 +2,8 @@ package money;
 
 public class Franc extends Money {
 
-	private String currency;
-
 	Franc(int amount, String currency) {
 		super(amount, currency);
-	}
-
-	Money times(int multiplier) {
-		return Money.franc(amount * multiplier);
 	}
 
 }
