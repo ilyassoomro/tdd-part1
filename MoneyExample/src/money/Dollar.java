73f397d30c17ce1f190dae8eb82757da3921f2ea
ilyassoomro <muhammadilyas_bsel@yahoo.com>
2015-01-04 17:53:22 +0500

    Chapter 9. Times We're Livin' In

--- MoneyExample/src/money/Dollar.java
+++ MoneyExample/src/money/Dollar.java
@@ -3,11 +3,14 @@ package money;
 
 public class Dollar extends Money{
 
-	Dollar(int amount) {
-		this.amount= amount;
+	private String currency;
+
+	Dollar(int amount, String currency) {
+		super(amount, currency);
 	}
 
 	Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return Money.dollar(amount * multiplier);
 	}
+
 }
